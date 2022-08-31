80006e47d5b3fb11bc4bbe511fa5b7c792a46ec4
Pedro Bigas <91469236+PedroBigas@users.noreply.github.com>
2022-08-31 17:59:20 -0300

    React-Router

---
+++ src/components/Sobre.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Sobre(props){
+
+    return(
+        <div className='page'>
+            <h1>Essa é Sobre a página</h1>
+        </div>
+    )
+}
